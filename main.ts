43fbfec81127e6b6a3ad5b72ada3c9c3f336579d
aaamtbs <76047300+aaamtbs@users.noreply.github.com>
2020-12-17 16:29:56 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+let micVal = 0
+let tree = ChristmasTree.create()
+tree.changeMode(LEDMode.Free)
 basic.forever(function () {
-	
+    micVal = input.soundLevel()
+    tree.equalizerAnimation(micVal)
+    tree.update()
 })
